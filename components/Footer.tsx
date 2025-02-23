a9bbccae6bb9bb2d9d9ac5238f9a0f7855f95a52
kameleyon <kameleyon@gmail.com>
2025-02-22 23:39:56 -0500

    Fixing Footer Landing page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="w-full text-white/80 py-3 px-6 text-sm border-t border-white/70 bg-primary/20 backdrop-blur-sm mt-18">
-      <div className="flex justify-between items-center">
+    <footer className="w-full text-white/80 py-3 px-6 text-sm border-t border-white/70 bg-gradient-to-r from-secondary to-accent relative">
+      <div className="absolute inset-0 dot-pattern bg-black opacity-10"></div>
+      <div className="flex justify-between items-center relative z-10">
         <p>2025. Copyright AstroGenie</p>
-        
       </div>
     </footer>
   );
